9f31c822bc0cd69608a140474ec64b7ccf939434
oshepotkin <oleg.shepotkin@artezio.com>
2017-03-03 17:53:51 +0300

    add react view

---
+++ HockeyFights/web/static/scripts/app/app.js
@@ -0,0 +1,9 @@
+﻿import React from 'react';
+import { render } from 'react-dom';
+import "node_modules/video-react/dist/video-react.css";
+
+
+
+import Index from './components/Index';
+
+render(<Index />, document.getElementById('js-main'));
---
+++ HockeyFights/webpack.config.js
@@ -0,0 +1,36 @@
+﻿var webpack = require('webpack');
+var path = require('path');
+module.exports = {
+    entry: [
+      "./web/static/scripts/app/app.js"
+    ],
+    output: {
+        path: __dirname + '/web/static',
+        filename: "bundle.js"
+    },
+    resolve: {
+        modules: [
+          path.join(__dirname, ''),
+          'node_modules'
+        ],
+        extensions: ['.webpack.js', '.web.js', '.js', '.jsx']
+    },
+    module: {
+        loaders: [
+          {
+              test: /\.js?$/,
+              loader: 'babel-loader',
+              query: {
+                  presets: ['es2015', 'react']
+              },
+              exclude: /node_modules/
+          },
+          {
+              test: /\.css?$/,
+              loader: "style-loader!css-loader"
+          }
+        ]
+    },
+    plugins: [
+    ]
+};
