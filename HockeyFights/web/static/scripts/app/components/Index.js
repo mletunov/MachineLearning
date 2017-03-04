fca77bb0b0127fc7c2551e3e7fc37a50b39253bf
Oleg Schepotkin <massive.spire.sound7@gmail.com>
2017-03-04 23:19:55 +0300

    daily progress

--- HockeyFights/web/static/scripts/app/components/Index.js
+++ HockeyFights/web/static/scripts/app/components/Index.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import { Player } from 'video-react';
 import Dropzone from 'react-dropzone';
 import request from 'superagent';
+import Loader from 'react-loader';
 
 class IndexComponent extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {timeStamps:[], videoSource:""};
+    this.state = {timeStamps:[], videoSource:"", loaded: true};
   }
 
   openUploadClick(){
@@ -15,6 +16,7 @@ class IndexComponent extends Component {
   }
 
   onDrop(files){
+        this.setState({loaded: false})
         var req = request.post('/upload');
         files.forEach((file)=> {
             req.attach('videoFile', file);
@@ -26,7 +28,8 @@ class IndexComponent extends Component {
 
     this.setState({videoSource: result.body.filePath})
     this.getTimeStamps(result.body.fileName)
-    this.refs.player.load();
+    if(this.player)
+      this.player.load();
   }
 
 
@@ -35,17 +38,19 @@ class IndexComponent extends Component {
       .then((response) =>{
           if (response.status !== 200) {
             console.log('Looks like there was a problem. Status Code: ' +  response.status);
+            this.setState({loaded: true});
             return;
           }
 
           response.json().then((stamps) =>
             {
-              this.setState({timeStamps: stamps});
+              this.setState({timeStamps: stamps, loaded: true});
             }
           );
         }
       )
       .catch(function(err) {
+        this.setState({loaded: true});
         console.log('Fetch Error :-S', err);
       });
   }
@@ -56,7 +61,8 @@ class IndexComponent extends Component {
   }
 
   goToStemp(seconds) {
-      this.refs.player.seek(seconds);
+      debugger;
+      this.player.seek(seconds);
   }
 
   render() {
@@ -67,8 +73,10 @@ class IndexComponent extends Component {
                             <button key={index} type="button" className={'btn time-stamp-button ' + (stamp.fightStart == true ? 'btn-danger' : 'btn-default btn-end')} onClick={() => this.goToStemp(stamp.timeStamp)}>{stamp.fightStart == true ? 'Fight': 'Fight end'}</button>
                           ) : null
 
+
     return (
       <div>
+
         <nav className="navbar navbar-default">
           <div className="container">
             <div className="navbar-header">
@@ -78,32 +86,35 @@ class IndexComponent extends Component {
             </div>
           </div>
         </nav>
-        <div className="container content-block">
-            <div className="col-md-3 uploader-block">
-              <Dropzone ref="dropUploader" onDrop={this.onDrop.bind(this)}>
-                  <div className="drop-text-block">
-                    <div className="drop-text">Drop videos here</div>
+          <div className="container content-block">
+              <Loader loaded={this.state.loaded}>
+                  <div className="col-md-3 uploader-block">
+                    <Dropzone ref="dropUploader" onDrop={this.onDrop.bind(this)}>
+                        <div className="drop-text-block">
+                          <div className="drop-text">Drop videos here</div>
+                        </div>
+                    </Dropzone>
+                    <button type="button" className="btn btn-primary upload-button" onClick={() => this.openUploadClick()}>
+                      Upload Video
+                    </button>
                   </div>
-              </Dropzone>
-              <button type="button" className="btn btn-primary upload-button" onClick={() => this.openUploadClick()}>
-                Upload Video
-              </button>
-            </div>
-            <div className="col-md-8">
-               <div>
-                  <Player
-                      playsInline
-                      ref="player"
-                      src={this.state.videoSource}
-                    />
-                </div>
-                <div className="time-stamps-row">
-                    <div className="btn-group" role="group">
-                      {timeButtons}
+                    <div className="col-md-8">
+                        <div>
+                            <Player
+                                playsInline
+                                ref={(player) => { this.player = player; }}
+                                src={this.state.videoSource}
+                              />
+                        </div>
+                        <div className="time-stamps-row">
+                            <div className="btn-group" role="group">
+                              {timeButtons}
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-        </div>
+                  </Loader>
+              </div>
+
       </div>
     );
   }
