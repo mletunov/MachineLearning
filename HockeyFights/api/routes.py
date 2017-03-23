f2024df6dafe109bcf852b744564f22db2d17174
Maxim Letunov <maxim.letunov@artezio.com>
2017-03-23 19:24:39 +0300

    fix flask API

--- HockeyFights/api/routes.py
+++ HockeyFights/api/routes.py
@@ -1,61 +1,59 @@
 from api import app
 
 import flask, os
-from flask_cors import CORS, cross_origin
-
 import random
 import string
 import learning
 
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+def ajax(func):
+    def wrapper(*args, **kwargs):
+        try:
+            dict = func(*args, **kwargs) or {}
+            return flask.jsonify(dict)
+        except Exception as ex:
+            dict = {'exception' : str(ex)}
+            return flask.make_response(flask.jsonify(dict), 500)
+
+    wrapper.__name__ = func.__name__
+    return wrapper
 
 char_set = string.ascii_lowercase + string.digits
 
 @app.route('/upload', methods=["POST"])
+@ajax
 def upload():
-    try:
-        fileName = '{0}{1}{2}'.format('zz', ''.join(random.sample(char_set, 8)), '.mp4')
-        path = os.path.join('web', app.config['UPLOAD_FOLDER'], fileName)
-        url = '/video/{0}'.format(fileName)
-        
-        file = request.files['videoFile']
+    fileName = '{0}{1}{2}'.format('zz', ''.join(random.sample(char_set, 8)), '.mp4')
+    path = os.path.join('api', app.config['UPLOAD_FOLDER'], fileName)
+    url = '/video/{0}'.format(fileName)
 
-        if file:
-            file.save(path)           
-            return flask.jsonify({'success': True , 'filePath': url, 'fileName': fileName})
-    except Exception as ex:
-        return flask.jsonify({'success': False, 'message': ex})
+    file = flask.request.files['videoFile']
+    file.save(path)
+    return {'filePath': url, 'fileName': fileName}
 
-@app.route('/video/<path:filename>')
-def download_file(filename):
-    return flask.send_from_directory(app.config['UPLOAD_FOLDER'],
-                               filename, as_attachment=True)
+@app.route('/video/<fileName>')
+def download_file(fileName):
+    return flask.send_from_directory(app.config['UPLOAD_FOLDER'], fileName, as_attachment=True)
 
-@app.route('/time/<path:filename>')
-def get_time_stamps(filename):
-    try:
-        stamps = list();
-        for count in range(40):
-            stamps.append({
-                   'fightStart': count % 2 == 0,
-                   'timeStamp': count * 10 
-                })
-        return flask.jsonify(stamps);
-    except Exception as ex:
-        return flask.jsonify({'success': False, 'message': ex})
+@app.route('/time/<path:fileName>')
+@ajax
+def get_time_stamps(fileName):
+    stamps = [];
+    for count in range(40):
+        stamps.append({
+                'fightStart': count % 2 == 0,
+                'timeStamp': count * 10 
+            })
+
+    return stamps
 
 @app.route('/test')
+@ajax
 def test():
     path = os.path.join('web', app.config['UPLOAD_FOLDER'], flask.request.args.get('fileName'))
     import cv2
     
     predictor = app.config['predictor']
     
-    #learning.factory.video_dataset(path)
-    try:
-        cap = cv2.VideoCapture(path)
-        ret = cap.isOpened();
-        return flask.jsonify({'success': True})
-    except Exception as ex:
-        return flask.jsonify({'success': False, 'message': ex})
+    learning.factory.video_dataset(path)
+    cap = cv2.VideoCapture(path)
+    ret = cap.isOpened();
